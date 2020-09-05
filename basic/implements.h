e7d815f3e5c09ad1a0c1e103be807784cb244630
Evan Clegern <evanclegern.work@gmail.com>
2020-09-05 00:55:31 -0600

    Moved and documented

--- basic/implements.h
+++ basic/implements.h
@@ -1,9 +1,12 @@
+/*
+FILE: implements.h
+TITLE: Base Engine-IECAI Implentations
+PURPOSE: Provide 'hollow functions' to interface with target game engine.
+REVISION: 3
+*/
 #define __IECAI_ENGINE_IMPLEMENT__
-#include "base-struct.h"
-using namespace base;
-//Please complete these functions with your engine's back-end for them.
-//All are for the NPCs that we're working with. The int& is addresses of objects representable in the program.
-//I'm from the US, but, I prefer Metric. ALL UNITS ARE METIRC! (meters, liters, kilograms, etc)
+#include "base_structure.h"
+using namespace base; //Reference the Base Structure Provider
 void setAnimation(int& id, const char* what) {}
 void translate(int& id, point to) {}
 void rotate(int& id, float degrees) {}
