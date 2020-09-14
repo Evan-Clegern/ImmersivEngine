79372b8f09d5b9429878bf18293b623c77e7fee3
Evan Clegern <evanclegern.work@gmail.com>
2020-09-13 23:07:41 -0600

    Minor overhaul, update
    Added system for defining terrain. Slices are 5x5 square of points, linked together; has some stats for height changes and "obscuration," for AI visibility. Chunks consist of 15 x 15 of these slices, making a total of 75x75 points per chunk; similar to slices, but also with extra stats for AI calculation. Begun work on entity base.

--- basic/base_entity.h
+++ basic/base_entity.h
@@ -2,22 +2,50 @@
 FILE: base_entity.h
 TITLE: Basic Entity Layout Provider
 PURPOSE: Provides format bases for entities
-VERSION: 1
+VERSION: 2
+HEAD DEVELOPER NOTE: You may notice some similarities to Source-my first mapping project's engine
 */
+#define __IECAI_BASE_ENTITY__
+#include <vector>
+#include <string>
 namespace entbase {
 	struct point {
 		float posX,posY,posZ;
-		unsigned int pitch, yaw, roll;
-		//Pitch = X-Z; forward rotation
-		//Yaw = X-Y; top rotation
-		//Roll = Y-Z; side rotation
+		point(float x, float y, float z) : posX(x), posY(y), posZ(z) {}
 	};
-	namespace geometries {
-		struct rectPrism {
-			point a,b,c,d,e,f,g,h;
-		};
-		struct pyramid {
-			point ba,bb,bc,bd,top;
-		};
+	point p(float x, float y, float z) {
+		point bob(x, y, x);
+		return bob;
 	}
+	struct linked_point { 
+		//More or less enables us to have multi-dimensional and defined connections
+		point& base;
+		std::vector<point&> linkedTo;
+	};
+	struct brush_world {
+		vector<linked_point> points;
+		point midpoint;
+		bool solid, swimmable, flyable, destroyable;
+	};
+	struct terrain_slice {
+		//This needs to be a 5x5 square, where Z has the variance.
+		linked_point* points[24];
+		unsigned short int chunkChildID, obscuration;
+		//Obscuration is 0-100; helps with visibilty calculations
+		float avgHgt, maxHgt, minHgt, variance;
+	};
+	struct terrain_chunk {
+		//15x15 of terrain_slices; therefore, 75x75 points
+		terrain_slice* slices[224];
+		unsigned int chunkNum;
+		float elevation, maxHgt, minHgt, avgHgt, varianceHgt, xfrom, yfrom, avgObscuration;
+	};
+	class entBase {
+	protected:
+		bool solid, hint;
+		vector<linked_point> occupiedSpaceLocal;
+		//These are the +/- from the midpoint of the functional entity.
+	public:
+		entBa
+	};
 }
