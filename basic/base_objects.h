5ca3f1632765551aefc7be06954abb26d1628812
Evan Clegern <evanclegern.work@gmail.com>
2020-09-18 13:45:58 -0600

    Delete base_objects.h

--- basic/base_objects.h
+++
@@ -1,9 +0,0 @@
-/*
-FILE: base_objects.h
-TITLE: Basic Object provider
-PURPOSE: Build off entities to provide more functionality.
-VERSION: 1
-*/
-#include "base_entity.h"
-using namespace std;
-using namespace entbase;
