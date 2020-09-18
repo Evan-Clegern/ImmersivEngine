c42009c7b22990936b410de88ba8f1ab141a8037
Evan Clegern <evanclegern.work@gmail.com>
2020-09-18 13:41:37 -0600

    Added value handling to entBase/entity

--- basic/base_entity.h
+++ basic/base_entity.h
@@ -40,19 +40,34 @@ namespace entbase {
 		unsigned int chunkNum, biomeTypeNum;
 		float maxHgt, minHgt, avgHgt, varianceHgt, avgObscuration, normalTemperature;
 	};
+	class entityValue {
+	protected:
+		std::string title, defaultValue;
+		bool numeric, boolean;
+	};
 	class entBase {
 	protected:
 		bool solid, hint, ai, model, control, other;
 		std::vector<linked_point> occupiedSpaceLocal;
 		float volume;
 		std::string base_name;
+		std::vector<entityValue> values;
 		//These are the +/- from the midpoint of the functional entity.
 	public:
 		std::vector<int&> addressChildren;
-		entBase(bool _s, bool _h, bool _ai, bool _m, std::string _entname, std::vector<linked_point> pnt) : solid(_s), hint(_h), ai(_ai), model(_m) {
+		entBase(bool _s, bool _h, bool _ai, bool _m, bool _c, bool _oth, std::string _entname, std::vector<linked_point> pnt, std::vector<entityValue> neededVals) {
+			solid = _s;
+			hint = _h;
+			ai = _ai;
+			model = _m;
+			control = _c;
+			other = _oth;
 			base_name = _entname;
-			for (int i = 0; i < pnt.size() - 1; i++) {
+			for (int i = 0; i < pnt.length() - 1; i++) {
 				occupiedSpaceLocal.push_back(pnt.at(i));
+			}
+			for (int i = 0; i < neededVals.length() - 1; i++) {
+				values.push_back(neededVals.at(i));
 			}
 		}
 	};
@@ -61,21 +76,26 @@ namespace entbase {
 		entBase& base;
 		std::vector<linked_point> occupied_space;
 		point position;
+		std::string name;
 		int baseID;
 	public:
+		std::vector<std::string> data_list; //The 'curValue' for each of the base's required items
 		int registerSelf() {
 			int& ref = (int&)this;
-			entBase.addressChildren.push_back(ref);
+			this.base->addressChildren.push_back(ref);
 			return entBase.addressChildren.length() - 1;
 		}
-		entity(entBase& parent, point location) {
+		entity(entBase& parent, point location, std::string title, std::vector<std::string> list) : name(title) {
 			std::vector<linked_point> d = parent->occupiedSpaceLocal;
 			for (int i = 0; i < d.length() - 1; i++) {
-				this.occupied_space.push_back(d.at(i) + location);
+				occupied_space.push_back(d.at(i) + location);
 			}
-			this.position = location;
-			this.base = parent;
+			position = location;
+			base = parent;
 			registerSelf();
+			for (int i = 0; i < list.length() - 1; i++) {
+				data_list.push_back(list.at(i));
+			}
 		}
 		~entity() {
 			this.occupied_space.erase(this.occupied_space.begin(), this.occupied_space.end());
