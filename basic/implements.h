7457223580cadf554775b923e67a62ef6448184c
Evan Clegern <evanclegern.work@gmail.com>
2020-09-18 14:54:30 -0600

    Update implements.h

--- basic/implements.h
+++ basic/implements.h
@@ -5,12 +5,14 @@ PURPOSE: Provide 'hollow functions' to interface with target game engine.
 REVISION: 3
 */
 #define __IECAI_ENGINE_IMPLEMENT__
-#include "base_structure.h"
+#include "base_entity.h"
 using namespace base; //Reference the Base Structure Provider
-void setAnimation(int& id, const char* what) {}
-void translate(int& id, point to) {}
-void rotate(int& id, float degrees) {}
-point getPosition(int& id) {}
-list<point> getNearbyTerrain(int& id, float range) {}
-list<int&> getNearbyObjects(int& id, float range) {}
-list<int&> getNearbyNPCs(int& id, float range) {}
+using namespace std;
+//I would recommend using custom properties with each item to provide for the 'entity&' data point.
+void updateMidpoint(entity& obj) {}
+void setAnimation(entity& obj, string what) {}
+terrain_chunk getSurroundingTerrain(point at) {}
+vector<entity&> findSurroundingEntities(point at, int range) {}
+void setEngineOValue(entity& obj, string where, string value) {}
+string getEngineOValue(entity& obj, string where) {}
+void triggerEvent(string what, string data) {}
