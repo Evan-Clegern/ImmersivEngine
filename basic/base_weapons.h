49af78bf9d65538ab296fb9f0338ec3afc1b94da
Evan Clegern <evanclegern.work@gmail.com>
2020-09-20 13:02:14 -0600

    Create base_weapons.h

---
+++ basic/base_weapons.h
@@ -0,0 +1,15 @@
+/*
+FILE: base_weapons.h
+TITLE: Base NPC Weapons Provider
+PURPOSE: Provide bases for equipable weapons.
+REVISION: 1
+*/
+#include "base_entity.h"
+
+namespace weapons {
+	namespace data {
+		enum dmgType {};
+		
+	}
+	
+}
