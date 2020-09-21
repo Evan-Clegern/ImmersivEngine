f0d6b57c317e0026bd1ed5a07eb9c4b25f4618b1
Evan Clegern <evanclegern.work@gmail.com>
2020-09-21 14:43:06 -0600

    Update and rename base_weapons.h to base_stats.h

---
+++ basic/base_stats.h
@@ -0,0 +1,54 @@
+/*
+FILE: base_stats.h
+TITLE: Base NPC Stat System Provider
+PURPOSE: Provide bases for health and other statistics involved in NPC actions
+REVISION: 3
+*/
+//Note: all distance-like stats are in POINTS.
+namespace statSys {
+	struct basicstats {
+		float health, stamina, speed, jumpHeight, cleverness, riskiness, sightRange, moodLevel, sleepiness;
+		//Cleverness, SighRange and Riskiness are CRITICAL for NPC behavior algorithms.
+		bool angry, sad, inCombat, scared, emboldened;
+		//These booleans, and the 'moodLevel' and 'sleepiness,' affect other algorithms and emotive behaviors.
+		int typeResist[11]; //Resistances to specific damage types, simplified to an integer.
+	};
+	struct advancedStats {
+		//For more open-world environments; optional.
+		basicstats base;
+		float hunger, thirst, weight, hgrRegenDrain, thrRegenDrain, regenPerSec, hearingRange, smellingRange;
+	};
+	namespace damage {
+		enum dmgType {BLUNT, SHARP, GUNSHOT, EXPLOSIVE, HEAT, FIRE, COLD, ACID, TOXIN, BIOHAZARD, RADIATION, SHOCK};
+		struct dmg { //Non-targeted damage; more of a placeholder/template, especially for weapons.
+			dmgType type;
+			float amount;
+			bool critical;
+		};
+		struct damage : dmg { //Targeted damage
+			basicstats& inflictor, victim;
+		};
+		struct debuff {
+			dmg& onTickA, onTickB, onTickC;
+			float tickPerSecond, stamDrain, resistDrain, speedDrain, sleepAdd, moodDrain;
+			int secondsLength;
+			bool curable, lethal, instaRestore;
+			//iF not lethal, victim HP won't drop to 0. instaRestore puts *drained* stats back to full once done
+		};
+		void inflictDamage(damage data) { //Runs calculations to inflict the damage to the victim.
+			float dResist = 1 - (data.victim->typeResist[data.type] / 100); //Percentile steps of the damage type to ignore.
+			float dmg = data.amount;
+			if (data.critical) {
+				dResist=1 - (data.victim->typeResist[data.type] / 225); //Resistance is effectively MUCH lower
+				dmg*=1.1; //10% stronger atop reduced resistance
+			}
+			data.victim->health-=(dmg * dResist);
+		}
+		void inflictDmg(dmg damage, basicstats& victim, basicstats& inflictor) {
+			damage d;
+			d.victim = victim;
+			d.inflictor = inflictor;
+			inflictDamage(d);
+		}
+	}
+}
