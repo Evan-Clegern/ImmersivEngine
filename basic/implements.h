945013d90d45efc3fca1506af4427369f6511f5c
Evan Clegern <evanclegern.work@gmail.com>
2020-10-05 09:44:37 -0600

    Minor additions

--- basic/implements.h
+++ basic/implements.h
@@ -17,13 +17,13 @@ namespace npc_actions {
     //NOTE: For below - Start at the terrain at (at), and then work it out to the maximum 'X' chunk before "curling" around
     //Like this (A to I)
     // E D C 
-    // F A B
+    // F A B    >> vector<terrain_chunk&> {A,B,C,D,E,F,G,H,I};
     // G H I
     vector<terrain_chunk&> getSurroundingTerrainSquare(point at, int rangeInChunks) {}
     vector<entity&> findSurroundingEntities(point at, int range) {}
     void storeWep(entity& weilder, entity& what) {}
     void takeOutWep(entity& weilder, entity& what) {}
-    void updateStat(entity& npc, string what, float new) {}
+    void updateStat(entity& npc, string what, float newlvl) {}
     float getStat(entity& npc, string what) {}
     void kill(entity& npc, entity& inflictor) {}
 }
@@ -35,6 +35,7 @@ namespace eng_actions {
     void triggerEvent(string what, string data) {}
     void log(string text) {}
     void logAIonly(string text) {}
+    void destroy(entity& obj) {}
     void hide(entity& obj) {}
     void show(entity& obj) {}
 }
