fc21d69fcb74317f850d904100957bc2298261d1
Evan Clegern <evanclegern.work@gmail.com>
2020-11-05 15:39:04 -0700

    MAJOR OVERHAUL + FOUT COMPLETION!

--- basic/base_entity.h
+++ basic/base_entity.h
@@ -4,7 +4,7 @@ TITLE: Basic Entity Layout Provider
 PURPOSE: Provides format bases for entities
 VERSION: 22
 */
-#define __IECAI_FVERSION 1.2
+#define __IECAI_FVERSION 1.25
 #define __IECAI_BASE_ENTITY__
 #include <vector>
 #include <string>
@@ -14,28 +14,32 @@ VERSION: 22
 #include <json/writer.h>
 enum axi {x,y,z};
 class point {
-protected:
-	float posX,posY,posZ;
 public:
+	float posX,posY,posZ;
 	point(float x, float y, float z) : posX(x), posY(y), posZ(z) {}
-	inline point operator+(point a, point b) {
-		float pX,pY,pZ = a.posX + b.posX,a.posY+b.posY,a.posZ+b.posZ;
-		point temp(pX,pY,pZ);
-		return temp;
+	inline void operator+=(point b) const {
+		this.posX +=b.posX;
+		this.posY +=b.posY;
+		this.posZ +=b.posZ;
 	}
-	inline point operator++(point a, float hgt) {
-		float pX,pY,pZ = a.posX, a.posY, a.posZ + hgt;
-		point temp(pX, pY, pZ);
-		return temp;
-	}
-	inline float operator>>(point a, point b) {
-		//get distance
-		float xDist = pow(b.x - a.x, 2);
-		float yDist = pow(b.y - a.y, 2);
-		float zDist = pow(b.z - a.z, 2);
-		return sqrt(xDist + yDist + zDist);
+	inline void operator++(float hgt) const {
+		this.posZ += hgt;
 	}
 };
+//"throwback" function
+point operator+(point a, point b) {
+	float x = a.x + b.x;
+	float y = a.y + b.y;
+	float z = a.z + b.z;
+	return p(x,y,z);
+}
+float operator>>(point a, point b) {
+	//get distance between points
+	float xDist = pow(b.x - a.x, 2);
+	float yDist = pow(b.y - a.y, 2);
+	float zDist = pow(b.z - a.z, 2);
+	return sqrt(xDist + yDist + zDist);
+}
 point p(float x, float y, float z) {
 	point bob(x, y, x);
 	return bob;
@@ -61,21 +65,21 @@ namespace entbaseD {
 		float maxHgt, minHgt, avgHgt, varianceHgt, avgObscuration, normalTemperature;
 	};
 	class entityValue {
-	protected:
+	public:
 		std::string title, defaultValue;
 		bool numeric, boolean;
 	};
 	class entBase {
-	protected:
+	public:
+		//Treat these *like* they're read-only, please...
 		bool solid, hint, npc, model, control, other;
 		int npcclassID; //Set to -1 if N/A, set to &(NPC_class) if applicabale
 		std::vector<linked_point> occupiedSpaceLocal;
 		float volume;
 		std::string base_name;
 		std::vector<entityValue> values;
-		//These are the +/- from the midpoint of the functional entity.
-	public:
-		std::vector<int&> addressChildren;
+		//No more *readonly-ish*
+		std::vector<int> realtime_children;
 		entBase(bool _s, bool _h, bool _ai, bool _m, bool _c, bool _oth, std::string _entname, std::vector<linked_point> pnt, std::vector<entityValue> neededVals, float _vol) {
 			solid = _s;
 			hint = _h;
@@ -94,20 +98,22 @@ namespace entbaseD {
 		}
 	};
 	class entity {
-	protected:
+	public:
+		//Pretend that this section is protected
 		bool smellable, isCrouched;
 		entBase& base;
 		std::vector<linked_point> occupied_space;
-		point& position, effRotation;
+		point true_pos, rotation;
 		terrain_slice& relative_pos;
 		std::string name;
 		float height;
-		int baseID, engineID, linkedNPC, smellStrength; //Set linkedNPC to -1 if not applicable, set to &(npc) if applicable!
-	public:
+		int baseID, engineID, linkedNPC, smellStrength, realtimeID;
+		//Set linkedNPC to -1 if not applicable, set to the JSON ID if applicable!
+		//No more *pretend protected*
 		std::vector<std::string> data_list; //The 'curValue' for each of the base's required items
-		int registerSelf() {
-			int& ref = (int&)this;
-			this.base->addressChildren.push_back(ref);
+		int registerSelf(int file_id) {
+			this.realtimeID = file_id;
+			this.base->addressChildren.push_back(realtimeID);
 			return entBase.addressChildren.length() - 1;
 		}
 		entity(entBase& parent, point& location, std::string title, std::vector<std::string> list) : name(title) {
@@ -130,9 +136,10 @@ namespace entbaseD {
 			base=NULL;
 			delete baseID;
 		}
-		void updatePosition(point& pos, point& rot) {
+		void updatePosition(point pos, point rot) {
 			this.position = pos;
 			this.effRotation = rot;
+			//TODO: Terrain Slice (relative positioning)
 		}
 	};
 }
@@ -222,10 +229,14 @@ namespace entbaseFIN {
 			the_pnt.linkedTo = pointes;
 			return the_pnt;
 		}
-		std::vector<entbaseD::linked_point> fetch_alllinks(Json::Value obj, std::string spacelocal_name) {
+		std::vector<entbaseD::linked_point> fetch_alllinks(Json::Value obj) {
 			std::vector<entbaseD::linked_point> bruh;
-			for (int i = 0; i < list_const.size(); i++) { //i love you jsoncpp
-				const Json::Value list = obj[spacelocal_name][i];
+			std::string n = "spacelocal_s";
+			if (obj.get(n,-5).asInt() == 0) {
+				throw "No Linked points exist in class!";
+			}
+			for (int i = 0; i < obj.get(n, 1).asInt() - 1; i++) { //i love you jsoncpp
+				const Json::Value list = obj["spacelocal"][i];
 				const entbaseD::linked_point yuh = fetch_linkedPoint(list);
 				bruh.push_back(yuh);
 			}
@@ -254,7 +265,7 @@ namespace entbaseFIN {
 			int total = listA.size();
 			const Json::Value listB = entit["valuedata"];
 			std::vector<entbaseD::entityValue> burh;
-			for (int i = 0; i < total; i++) {
+			for (int i = 0; i < total - 1; i++) { //jsoncpp example says it may treat "size" with respect to index... I doubt it.
 				burh.push_back( get_entvalue(listB, listA[i]) );
 			}
 			return burh;
@@ -296,7 +307,6 @@ namespace entbaseFIN {
 				
 			}
 		}
-		//We're also going to need a way to make large batch jobs for linked points - they are god awful
 		bool ispointent(Json::Value fileoper, std::string name) {
 			float t = simple::f_fetchnested(fileoper, name, "volume");
 			if (t == 0.00) {return true;} else {return false};
@@ -316,7 +326,7 @@ namespace entbaseFIN {
 		_c = simple::i_listobj(operations, "options", 4);
 		_ot = simple::i_listobj(operations, "options", 5);
 		int classe = simple::i_fetchsingle(operations, "npc_class");
-		std::vector<entbaseD::linked_point> space = simple::fetch_alllinks(operations, "spacelocal");
+		std::vector<entbaseD::linked_point> space = simple::fetch_alllinks(operations);
 		std::vector<int> childfids= simple::i_fetchlist(operations, "children");
 		float vol = f_fetchsingle(operations, "volume");
 		std::vector<entbaseD::entityValue> vals = simple::load_values(operations);
@@ -331,8 +341,10 @@ namespace entbaseFOUT {
 		//JSONCPP example says you can... okay then
 		Json::Value oldstream = entbaseFIN::loadstream(file);
 		if (not entbaseFIN::f_tests::validmeta(oldstream, "entity")) {return false;}
-		Json::Value newstream;
-		std::fstream streame(file, std::fstream::in | std::fstream::out | std::fstream::binary);
+		Json::Value newstream = oldstream;
+		//We'll overwrite the entire file with this
+		//Why? We mirrored its contents in a Json::Value
+		std::fstream streame(file, std::fstream::in | std::fstream::out | std::fstream::binary | std::fstream::truncate);
 		newstream["metadata"]["iecai-vers"] = __IECAI_FVERSION;
 		newstream["metadata"]["file-vers"] = (oldstream["metadata"].get("file-vers",0).asInt() + 1);
 		//Once we determine if it's replacing or not, change this accordingly.
@@ -340,8 +352,68 @@ namespace entbaseFOUT {
 		if (oldstream.get(base->base_name, "") == NULL) {
 			newstream["metadata"]["ent-count"] = t + 1;
 		} else {
-			newstream["metadata"]["ent-count"] = t; //Object exists in previous file
+			newstream["metadata"]["ent-count"] = t; //Object exists in file
 		}
+		std::string name = base->base_name;
+		//Reverse Construct the entBase class.
+		newstream[name]["volume"] = base->volume;
+		newstream[name]["npc_class"] = base->npcclassID;
+		//THE BATCH CONSTRUCTOR FOR LINKED POINTS!!!!!!!!!!!!!!!!!!!!!!
+		for (int A = 0; A < base->occupiedSpaceLocal.size() - 1; A++) {
+			linked_point pnt = base->occupiedSpaceLocal.at(A);
+			newstream[name]["spacelocal"][A]["main"][0] = pnt.base.x;
+			newstream[name]["spacelocal"][A]["main"][1] = pnt.base.y;
+			newstream[name]["spacelocal"][A]["main"][2] = pnt.base.z;
+			int index = pnt.linkedTo.size;
+			newstream[name]["spacelocal"][A]["size"] = index;
+			for (int i = 0; i < index - 1; i++) {
+				const point D = pnt.linkedTo.at(i);
+				//oh what fun. 5-level parenting...
+				newstream[name]["spacelocal"][A]["links"][i][0] = D.x;
+				newstream[name]["spacelocal"][A]["links"][i][1] = D.y;
+				newstream[name]["spacelocal"][A]["links"][i][2] = D.z;
+			}
+			newstream[name]["spacelocal_s"] = index;
+			delete &pnt;
+		}	
+		for (int i = 0; i < base->values.size() - 1; i++) {
+			entbaseD::entityValue d = base->values.at(i);
+			newstream[name]["valuenames"][i] = d.title;
+			int deed;
+			if (d.numeric) {
+				deed = 2;
+			} else if (d.boolean) {
+				deed = 1;
+			} else {
+				deed = 0;
+			}
+			newstream[name]["valuedata"][d.title][0] = deed; //0 = string, 1 = bool (Yes/No), 2 = number (Float)
+			newstream[name]["valuedata"][d.title][1] = d.defaultValue;
+		}
+		for (int i = 0; base->addressChildren.size() - 1; i++) {
+			newstream[name]["children"][i] = base->addressChildren.at(i);
+		}
+		//Last thing: options
+		int o_a, o_b, o_c, o_d, o_e, o_f;
+		if (base->solid) {o_a = 1;} else {o_a = 0;}
+		if (base->hint) {o_b = 1;} else {o_b = 0;}
+		if (base->npc) {o_c = 1;} else {o_c = 0;}
+		if (base->model) {o_d = 1;} else {o_d = 0;}
+		if (base->control) {o_e = 1;} else {o_e = 0;}
+		if (base->other) {o_f = 1;} else {o_f = 0;}
+		newstream[name]["options"][0] = o_a;
+		newstream[name]["options"][1] = o_b;
+		newstream[name]["options"][2] = o_c;
+		newstream[name]["options"][3] = o_d;
+		newstream[name]["options"][4] = o_e;
+		newstream[name]["options"][5] = o_f;
+		delete &oldstream;
+		try {
+			streame << newstream;	
+		} catch {
+			return false;	
+		}
+		return true;
 	}
 	
 }
