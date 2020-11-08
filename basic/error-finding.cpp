f04a349561d5f6e8c8d711237dd586321884b000
Evan Clegern <evanclegern.work@gmail.com>
2020-11-08 11:13:06 -0700

    Create error-finding.cpp

---
+++ basic/error-finding.cpp
@@ -0,0 +1,31 @@
+/*
+TESTING FILE: November 2020
+FILE: error-finding.cpp
+PURPOSE: Find errors with the functions of base_entity.h
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <json/json.h>
+#include "base_entity.h"
+using namespace std;
+using namespace Json;
+using namespace entbaseD;
+using namespace entbaseFIN;
+using namespace entbaseFOUT;
+//Date : 8 November 2020
+void test_pnt() {
+  point d = p(185.30, -184.13, 10.2);
+  point e = p(100.34, -10.66, 0.27);
+  float goop = d >> e;
+  point added = d + e;
+  point g = added;
+  added++ goop;
+}
+
+int main() {
+  test_pnt();
+  return 0;
+}
