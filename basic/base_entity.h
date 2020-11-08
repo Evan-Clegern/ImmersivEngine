ca8b8f7f719ec6e0e26af4539a464d8a8a7ed754
Evan Clegern <evanclegern.work@gmail.com>
2020-11-08 14:46:07 -0700

    Round 4 Fixes + Documentation

--- basic/base_entity.h
+++ basic/base_entity.h
@@ -25,7 +25,8 @@ public:
 		float y = this->posY + b.posY;
 		float z = this->posZ + b.posZ;
 		//Round 3 Fixes: 'this' is treated as readonly
-		return obje(x,y,z);
+		point obje(x,y,z); //Round 4 Fix: moved constructor to before return statement
+		return obje;
 	}
 };
 point p(float x, float y, float z) { //Round 2 Fix: Moved up
@@ -33,13 +34,14 @@ point p(float x, float y, float z) { //Round 2 Fix: Moved up
 	return bob;
 }
 //"throwback" function
-point operator+(point a, point b) {
+point point::operator+(point a, point b) {
 	float x = a.posX + b.posX;
 	float y = a.posY + b.posY;
 	float z = a.posZ + b.posZ;
 	return p(x,y,z);
 }
-float operator>>(point a, point b) {
+//Round 4 Fixes/Cleanup: Put these operators as children of 'point' function.
+float point::operator>>(point a, point b) {
 	//get distance between points
 	float xDist = pow(b.posX - a.posX, 2);
 	float yDist = pow(b.posY - a.posY, 2);
@@ -52,17 +54,25 @@ namespace entbaseD {
 		//More or less enables us to have multi-dimensional and defined connections
 		point base;
 		std::vector<point> linkedTo;
-		inline linked_point operator+(point in) {
-			linked_point t;
-			t.base = this->base + in;
-			for (int ind = 0; ind < linkedTo.size() -1; ind++) {
-				point yuh = this->linkedTo.at(ind);
-				yuh += in;
-				t.linkedTo.push_back(yuh);
-			}
-			return t;
+		linked_point(point _b, std::vector<point> _to) {
+			base = _b;
+			linkedTo = _to;
 		}
 	};
+	//Round 4 Cleanup: moved target debug function out of the class definition
+	inline linked_point linked_point::operator+(point in) {
+		point baes = this->base + in;
+		std::vector<point> points;
+		//Resolved Issue A8: use of deleted function [constructor]
+		for (int ind = 0; ind < linkedTo.size() -1; ind++) {
+			point yuh = this->linkedTo.at(ind);
+			yuh += in;
+			points.linkedTo.push_back(yuh);
+		}
+		//Round 4 Fix: applied the linked_point constructor
+		linked_point t(baes, points);
+		return t;
+	}
 	//Very simple groups; put here so they aren't cluttering other header files
 	struct terrain_slice {
 		//This needs to be a 5x5 square, where Z has the variance.
@@ -128,26 +138,31 @@ namespace entbaseD {
 		//No more *pretend protected*
 		std::vector<std::string> data_list; //The 'curValue' for each of the base's required item
 		int registerSelf(int file_id) {
+			//Round 4 Fixes: replaced 'addressChildren' with new name 'realtime_children'
 			this->realtimeID = file_id;
 			//Round 2 Fix(es): (const entity*)this  fix (-> versus .)
-			this->base.addressChildren.push_back(realtimeID);
+			this->base.realtime_children.push_back(realtimeID);
 			//Round 2 Fix: Naming issue, and -> versus .
 			//Round 3 Fix: 'base' no longer a pointer when called by this->base
-			return this->base.addressChildren.length() - 1;
+			return this->base.realtime_children.length() - 1;
 		}
-		entity(entBase& parent, point location, terrain_slice& ter_location, std::string title, std::vector<std::string> list, int realID) : base(parent), relative_pos(ter_location) {
+		entity(entBase& parent, point& location, terrain_slice& ter_location, std::string title, std::vector<std::string> list, int realID) : base(parent), relative_pos(ter_location), true_pos(location) {
 			//Round 2 Fix: Replaced 'point& location' to 'point location'
 			//Round 3 Fix: Replaced position of title set
 			name = title;
-			//Issue A1: uninitialized reference member to 'class entbaseD::entBase&'
-			//Issue A2: no matching function for call to 'point::point()'
-			//Issue A3: uninitialized reference member to 'struct entbaseD::terrain_slice&'
-			//Round 3 Potential Fix (A1 & A3): Included before-function declarations
-			//Issue A4: base operand of '->' has non-pointer type 'entbaseD::entbase'
+			/*
+			Resolved Issue A1: uninitialized reference member to 'class entbaseD::entBase&'
+			Issue A2: no matching function for call to 'point::point()'
+			Round 4 Potential Fix: Made location a pointer; moved to before-function declare
+			Issue A2 does not make sense; we're not using the point constructor here???
+			Resolved Issue A3: uninitialized reference member to 'struct entbaseD::terrain_slice&'
+			Round 3 Fix (A1 & A3): Included before-function declarations
+			Resolved Issue A4: base operand of '->' has non-pointer type 'entbaseD::entbase'
+			*/
 			std::vector<linked_point> d = parent->occupiedSpaceLocal;
 			//Round 2 Fix: forgot that vector doesn't have 'length' but 'size'
 			for (int i = 0; i < d.size() - 1; i++) {
-				//Issue A5: no match for operator+
+				//Resolved Issue A5: no match for operator+
 				//It's because d.at(i) is a .. linked_point
 				//Maybe make a function for a linked_point to fix?
 				occupied_space.push_back(d.at(i) + location);
@@ -158,7 +173,7 @@ namespace entbaseD {
 			rotation = p(0.0,0.0,0.0);
 			base = parent;
 			registerSelf(realID);
-			//Issue A6: no matchng function for call to 'entbaseD::entity::registerSelf()'
+			//Resolved Issue A6: no matching function for call to 'entbaseD::entity::registerSelf()'
 			//Round 3 Potential Fix (A6): added 'realID' to the constructor for this function
 			//Round 2 Fix: forgot that vector doesn't have 'length' but 'size'
 			for (int i = 0; i < list.size() - 1; i++) {
@@ -168,7 +183,9 @@ namespace entbaseD {
 		~entity() {
 			//Round 2 Fixes: replaced the bad 'this.' with 'this->'
 			this->occupied_space.erase(this->occupied_space.begin(), this->occupied_space.end());
-			this->base->addressChildren.erase(this->base->addressChildren.begin() + this->baseID);
+			//Round 4 Fix: Renamed 'addressChildren' to 'realtime_children'
+			//Round 4 Fix: turned -> to . after 'this' (reason not really clear but it works)
+			this->base.realtime_children.erase(this->base.realtime_children.begin() + this->baseID);
 			delete &(this->occupied_space); //Round 2 Fixes: implemented pointers
 			//Round 3 Fix: Purged un-needed base clear and baseId clear
 		}
@@ -186,6 +203,7 @@ namespace entbaseFIN {
 		std::string s_fetchsingle(Json::Value input, std::string name) {
 			//Issue A7: no matching function call to 'Json::Value::get(Json::Value)' ?
 			//Unknown reason; this follows jsoncpp's example code..
+			//A7 is not showing up in the error; but it doesn't *seem* resolved...
 			return input.get(name,"no").asString();
 		}
 		std::string s_fetchnested(Json::Value input, std::string parent, std::string name) {
@@ -226,13 +244,16 @@ namespace entbaseFIN {
 			return data;
 		}
 		std::string s_doublenested(Json::Value input, std::string parA, std::string parB, std::string vName) {
-			return (input[parA][parB].get(name, "no")).asString();
+			//Round 4 Fix: forgot to change 'name' to 'vName'
+			return (input[parA][parB].get(vName, "no")).asString();
 		}
 		int i_doublenested(Json::Value input, std::string parA, std::string parB, std::string vName) {
-			return (input[parA][parB].get(name, 0)).asInt();
+			//Round 4 Fix: forgot to change 'name' to 'vName'
+			return (input[parA][parB].get(vName, 0)).asInt();
 		}
 		float f_doublenested(Json::Value input, std::string parA, std::string parB, std::string vName) {
-			return (input[parA][parB].get(name, 0)).asFloat();
+			//Round 4 Fix: forgot to change 'name' to 'vName'
+			return (input[parA][parB].get(vName, 0)).asFloat();
 		}
 		std::string s_listobj(Json::Value input, std::string lname, unsigned short int index) {
 			return (input[lname][index]).asString();
@@ -252,15 +273,16 @@ namespace entbaseFIN {
 			int size = oper_const.get(size, 3).asInt() - 1;
 			std::vector<point> pointes;
 			for (int i=0;i<size;i++) {
-				float x= oper_const["links"][i][0];
-				float y= oper_const["links"][i][1];
-				float z= oper_const["links"][i][2];
+				//Round 4 Fix: Forgot '.asFloat()' item
+				float x= oper_const["links"][i][0].asFloat();
+				float y= oper_const["links"][i][1].asFloat();
+				float z= oper_const["links"][i][2].asFloat();
 				point t = p(x, y, z);
 				pointes.push_back(t);
 			}
-			entbaseD::linked_point the_pnt;
-			the_pnt.base = bruh;
-			the_pnt.linkedTo = pointes;
+			//Resolved Issue A9: use of deleted fnction 'entbaseD::linked_point:linked_point()'
+			//Round 4 Fix: applied the linked_point constructor
+			entbaseD::linked_point the_pnt(bruh, pointes);
 			return the_pnt;
 		}
 		std::vector<entbaseD::linked_point> fetch_alllinks(Json::Value obj) {
@@ -270,7 +292,7 @@ namespace entbaseFIN {
 				throw "No Linked points exist in class!";
 			}
 			for (int i = 0; i < obj.get(n, 1).asInt() - 1; i++) { //i love you jsoncpp
-				const Json::Value list = obj["spacelocal"][i];
+				const Json::Value list = obj["spacelocal"][i];				
 				const entbaseD::linked_point yuh = fetch_linkedPoint(list);
 				bruh.push_back(yuh);
 			}
@@ -290,7 +312,9 @@ namespace entbaseFIN {
 				bro.numeric = true;
 				bro.boolean = false;
 			}
-			bro.defaultValue = obj[1];
+			bro.defaultValue = obj[1].asString();
+			//Issue A10: no match for 'operator=' (std::string and Json::Value)
+			//Round 4 Potential Fix: added 'asString()'
 			bro.title = name;
 			return bro;
 		}
@@ -300,13 +324,16 @@ namespace entbaseFIN {
 			const Json::Value listB = entit["valuedata"];
 			std::vector<entbaseD::entityValue> burh;
 			for (int i = 0; i < total - 1; i++) { //jsoncpp example says it may treat "size" with respect to index... I doubt it.
-				burh.push_back( get_entvalue(listB, listA[i]) );
+				burh.push_back( get_entvalue(listB, listA[i].asString()) );
+				//Issue A11: could not convert from 'const Json::Value' to 'std::string'
+				//Round 4 Potential Fix: added .asString() to index call
 			}
 			return burh;
 		}
 	}
 	enum entval_t {text, toggle, number};
-	const float good_throwbacks[1.15];
+	const float good_throwbacks[] = [1.15];
+	//Round 4 Fix: forgot to add the [] = [****] thing
 	const int throwback_cnt 1;
 	namespace f_tests {
 		bool validmeta(Json::Value fileoper, std::string purpose) {
