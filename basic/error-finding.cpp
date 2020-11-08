cb11fbbcce32f94e6559f77a3b22d90c25dac4d4
Evan Clegern <evanclegern.work@gmail.com>
2020-11-08 16:28:00 -0700

    Update error-finding.cpp

--- basic/error-finding.cpp
+++ basic/error-finding.cpp
@@ -8,7 +8,8 @@ PURPOSE: Find errors with the functions of base_entity.h
 #include <vector>
 #include <cmath>
 #include <fstream>
-#include <json/json.h>
+#include "dist/jsoncpp.cpp"
+#include "dist/json/json-forwards.h"
 #include "base_entity.h"
 using namespace std;
 using namespace Json;
@@ -16,16 +17,16 @@ using namespace entbaseD;
 using namespace entbaseFIN;
 using namespace entbaseFOUT;
 //Date : 8 November 2020
-void test_pnt() {
+point test_pnt() {
   point d = p(185.30, -184.13, 10.2);
   point e = p(100.34, -10.66, 0.27);
   float goop = d >> e;
   point added = d + e;
   point g = added;
-  //removed the non-existent call.
+  return g;
 }
 
 int main() {
-  test_pnt();
+  point bruh = test_pnt();
   return 0;
 }
