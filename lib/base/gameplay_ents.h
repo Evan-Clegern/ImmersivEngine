f2ba4479df48b74533ca110a957d0eb5b266920d
Evan Clegern <evanclegern.work@gmail.com>
2020-11-19 13:36:29 -0700

    Create gameplay_ents.h

---
+++ lib/base/gameplay_ents.h
@@ -0,0 +1,9 @@
+/*
+FILE: gameplay_ents.h
+TITLE: Extended Gameplay Entities File - Extending 'bsc_dat_ent.h'
+PURPOSE: Operational Entities for Basic Gameplay and Logic, providing the Base (Default) Entity Definitions.
+COMMENT: These objects are critical elements and which are meant to be constant defaults. Not using a JSON file for them for logical functionality.
+VERSION: 1
+*/
+#include "bsc_dat_ent.h"
+//Development coming soon!
