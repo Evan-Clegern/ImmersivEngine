29b579caa74eed0acdb2e86e9f8eeef03d7a89b4
Evan Clegern <evanclegern.work@gmail.com>
2020-11-23 13:38:26 -0700

    Update and rename lib/opengl-app/base.h to lib/render/base.h

---
+++ lib/render/base.h
@@ -0,0 +1,16 @@
+#define __IMVENG_OPENGL_IMPLEMENT
+//Import FreeGLUT
+#include <GL/glut.h>
+//Import GLAD
+#include <glad/glad.h>
+//Import basic OpenGL mathematics modules
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+//Import standard C++ modules
+#include <iostream> //Console logs
+#include <string>
+#include <vector>
+#include <cmath>
