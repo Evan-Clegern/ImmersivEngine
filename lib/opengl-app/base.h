cf758c40b0629d2d4e1080758f60b0165bc53269
Evan Clegern <evanclegern.work@gmail.com>
2020-11-23 13:13:36 -0700

    Update base.h

--- lib/opengl-app/base.h
+++ lib/opengl-app/base.h
@@ -1,2 +1,10 @@
-#defines __IMVENG_OPENGL_IMPLEMENT
+#define __IMVENG_OPENGL_IMPLEMENT
+//Import openGL mathematics modules
+//https://github.com/g-truc/glm
+#include "glm/vec3.hpp"
+#include "glm/vec4.hpp"
+#include "glm/mat4x4.hpp"
+#include "glm/ext/matrix_transform.hpp"
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/scalar_constants.hpp"
 //Beginning programming very soon!
